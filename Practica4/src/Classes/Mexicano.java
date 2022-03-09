87c2ee259f2ed2b77280594fa0c7616c017f5e0b
AndresBarrer <86137176+AndresBarrer@users.noreply.github.com>
2022-03-08 19:24:40 -0800

    Finished CURP method

--- Practica4/src/Classes/Mexicano.java
+++ Practica4/src/Classes/Mexicano.java
@@ -1,8 +1,8 @@
 package Classes;
 import java.util.Scanner;
-import java.util.Iterator;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Random;
+
 
 
 public class Mexicano{
@@ -16,48 +16,47 @@ public class Mexicano{
     //genero, entidad
     String genero, entidad;
     
-    boolean isKeyPresent;
+    boolean checkIsKeyPresent;
     
     Scanner scan = new Scanner(System.in);
     
     //entidad
     //Map<String, String> entidades = new HashMap<>();
-    static HashMap<String, String> entidades;
-    static{
-    entidades.put("AGUASCALIENTES", "AS");
-    entidades.put("BAJA CALIFORNIA SUR", "BS");
-    entidades.put("COAHUILA", "CL");
-    entidades.put("CHIAPAS", "CS");
-    entidades.put("DISTRITO FEDERAL", "DF");
-    entidades.put("GUANAJUATO", "GT");
-    entidades.put("HIDALGO", "HG");
-    entidades.put("MEXICO", "MC");
-    entidades.put("MORELOS", "MS");
-    entidades.put("NUEVO LEON", "NL");
-    entidades.put("PUEBLA", "PL");
-    entidades.put("QUINTANA ROO", "QR");
-    entidades.put("SINALOA", "SL");
-    entidades.put("TABASCO", "TC");
-    entidades.put("TLAXCALA","TL");
-    entidades.put("YUCATAN","YN");
-    entidades.put("EXTRANJERO","NE");
-    entidades.put("BAJA CALIFORNIA","BC");
-    entidades.put("CAMPECHE","CC");
-    entidades.put("COLIMA","CM");
-    entidades.put("CHIHUAHUA","CH");
-    entidades.put("DURANGO","DG");
-    entidades.put("GUERRERO","GR");
-    entidades.put("JALISCO","JC");
-    entidades.put("MICHOACAN","MN");
-    entidades.put("NAYARIT","NT");
-    entidades.put("OAXACA","OC");
-    entidades.put("QUERETARO","QT");
-    entidades.put("SAN LUIS POTOSI","SP");
-    entidades.put("SONORA","SR");
-    entidades.put("TAMAULIPAS","TS");
-    entidades.put("VERACRUZ","VZ");
-    entidades.put("ZACATECAS","ZS");
-    }
+    HashMap<String, String> entidades = new HashMap<String, String>(){{
+    put("AGUASCALIENTES", "AS");
+    put("BAJA CALIFORNIA SUR", "BS");
+    put("COAHUILA", "CL");
+    put("CHIAPAS", "CS");
+    put("DISTRITO FEDERAL", "DF");
+    put("GUANAJUATO", "GT");
+    put("HIDALGO", "HG");
+    put("MEXICO", "MC");
+    put("MORELOS", "MS");
+    put("NUEVO LEON", "NL");
+    put("PUEBLA", "PL");
+    put("QUINTANA ROO", "QR");
+    put("SINALOA", "SL");
+    put("TABASCO", "TC");
+    put("TLAXCALA","TL");
+    put("YUCATAN","YN");
+    put("EXTRANJERO","NE");
+    put("BAJA CALIFORNIA","BC");
+    put("CAMPECHE","CC");
+    put("COLIMA","CM");
+    put("CHIHUAHUA","CH");
+    put("DURANGO","DG");
+    put("GUERRERO","GR");
+    put("JALISCO","JC");
+    put("MICHOACAN","MN");
+    put("NAYARIT","NT");
+    put("OAXACA","OC");
+    put("QUERETARO","QT");
+    put("SAN LUIS POTOSI","SP");
+    put("SONORA","SR");
+    put("TAMAULIPAS","TS");
+    put("VERACRUZ","VZ");
+    put("ZACATECAS","ZS");
+    }};
 
     public String getNombre(){
         return this.nombre;
@@ -127,39 +126,81 @@ public class Mexicano{
     }
     public void setEntidad(String entidad){
         this.entidad = entidad.toUpperCase();
-    }
 
-    isKeyPresent = entidades.containsKey(this.entidad);
-    if(isKeyPresent){
-        for (String key : entidades.keySet()) {
-            this.entidad = entidades.get(key);
+        //if the key is present, the continue inside condition
+        if(isKeyPresent(entidad)){
+
+            //for each key in the entire key set of the dictionary
+            for (String key : entidades.keySet()){
+                
+                //compares key string to the entered entidad string
+                if(key.equals(this.entidad)) {
+
+                    //assigns the found key value
+                    this.entidad = entidades.get(key.toUpperCase());
+                }
+            }
+        }else{
+            System.out.println("Entidad no valida.");
         }
-    }else{
-        System.out.println("Entidad no valida.");
     }
 
+    public boolean isKeyPresent(String entidad){
+        checkIsKeyPresent = entidades.containsKey(entidad.toUpperCase());
+        return checkIsKeyPresent;
+    }    
+    
     //siguente consonante del primer apellido
     public char getConsonantePrimerApellido(){
-        for(int i=0; i<this.apellido1.length(); i++) {
-            if(this.apellido1.charAt(i) != 'A'|| this.apellido1.charAt(i) != 'E'|| 
-                this.apellido1.charAt(i) != 'I' || this.apellido1.charAt(i) != 'O' || 
+        for(int i=1; i<this.apellido1.length(); i++) {
+            if(this.apellido1.charAt(i) != 'A' && this.apellido1.charAt(i) !='E'&& 
+                this.apellido1.charAt(i) != 'I' && this.apellido1.charAt(i) != 'O' && 
                 this.apellido1.charAt(i) != 'U') {
-
+                    System.out.println(this.apellido1.charAt(i) != 'A');
                     return this.apellido1.charAt(i);
             }
         }
+        return 0;
     }
 
     //siguente consonante del segundo apellido
-
+    public char getConsonanteSegundoApellido(){
+        for(int i=1; i<this.apellido2.length(); i++) {
+            if(this.apellido2.charAt(i) != 'A' && this.apellido2.charAt(i) !='E'&& 
+                this.apellido2.charAt(i) != 'I' && this.apellido2.charAt(i) != 'O' && 
+                this.apellido2.charAt(i) != 'U') {
+                    System.out.println(this.apellido2.charAt(i) != 'A');
+                    return this.apellido2.charAt(i);
+            }
+        }
+        return 0;
+    }
     
     //siguente consonante del primer nombre
-
+    public char getConsonantePrimerNombre(){
+        for(int i=1; i<this.nombre.length(); i++) {
+            if(this.nombre.charAt(i) != 'A' && this.nombre.charAt(i) !='E'&& 
+                this.nombre.charAt(i) != 'I' && this.nombre.charAt(i) != 'O' && 
+                this.nombre.charAt(i) != 'U') {
+                    System.out.println(this.nombre.charAt(i) != 'A');
+                    return this.nombre.charAt(i);
+            }
+        }
+        return 0;
+    }
 
     //homoclave
     //String nombre, String apellido1, String apellido2
+    Random Random = new Random();
+    char randomizedChar1 = (char) (Random.nextInt(43) + '0');
+    char randomizedChar2 = (char) (Random.nextInt(43) + '0');
     public void makeCURP(String apellido1, String apellido2, String nombre, String year, String mes, String dia, String genero, String entidad){
-        System.out.printf("\nCURP: " + this.apellido1.charAt(0) + this.getPrimerVocal() + this.apellido2.charAt(0) + this.nombre.charAt(0) + this.year + this.mes + this.dia);
+        System.out.printf("\nCURP: " + this.apellido1.charAt(0) + 
+                            this.getPrimerVocal() + this.apellido2.charAt(0) + 
+                            this.nombre.charAt(0) + this.year + this.mes + this.dia +
+                            this.genero + this.entidad + this.getConsonantePrimerApellido() + 
+                            this.getConsonanteSegundoApellido() + this.getConsonantePrimerNombre() +
+                            randomizedChar1 + randomizedChar2);
     }
-    
 }
+    
