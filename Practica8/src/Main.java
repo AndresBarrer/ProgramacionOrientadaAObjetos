0c51948c0969ea2439350c8489edb4fd5df2b01d
AndresBarrer <86137176+AndresBarrer@users.noreply.github.com>
2022-04-25 20:44:32 -0700

    Formatting changes

--- Practica8/src/Main.java
+++ Practica8/src/Main.java
@@ -25,12 +25,13 @@ public class Main {
             System.out.println("Division: " + t.getDivision(i));
             System.out.println("Puntos anotados: " + t.getPuntosEquipos(i));
             System.out.println("Cantidad de jugadores: " + t.getNumeroJugadores(i));
-            System.out.println("Jugadores:\n");
+            System.out.println("Jugadores:");
 
             for(int j=0; j<t.getNumeroEquiposParticipantes(); j++){
                 System.out.println("Nombre: " + t.getNombreJugador(i, j));
                 System.out.println("Numero: "+ t.getNumeroUniforme(i, j));
             }
+            System.out.println("\n");
         }
     }    
 }
